899a685b88848040dbd07c2b3e40816937f4697b
Sebastian Rosik <sebastian.rosik@gmail.com>
2017-02-05 22:36:31 +0100

    Changed env variables names

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require('dotenv').config({
   path: `${__dirname}/.env`
 });
 require('./server').start().then(app => {
-  const host = process.env.APP_HOST;
-  const port = process.env.APP_PORT;
+  const host = process.env.HOST;
+  const port = process.env.PORT;
   app.listen(port, host, () => {
     console.info(`Service is running on http://${host}:${port}`);
   });
