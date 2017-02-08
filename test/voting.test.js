0e515261c077f553e5648bd59356c5d03d84a149
Sebastian Rosik <sebastian.rosik@gmail.com>
2017-02-08 21:06:24 +0100

    Changing how voting works. Disallowing voting for your own team.

--- test/voting.test.js
+++ test/voting.test.js
@@ -5,40 +5,65 @@ require('dotenv').config({
 const {
   HOST,
   PORT,
-  TEST_OAUTH_TOKEN
+  TEST_OAUTH_TOKEN,
+  TEST_PERSON_GITHUB_ID,
+  TEST_PERSON_EMAIL
 } = process.env;
 
 const baseUrl = `http://${HOST}:${PORT}/api/v1`;
 const chakram = require('chakram');
-const uuidV4 = require('uuid/v4');
 const expect = chakram.expect;
 
 chakram.setRequestDefaults({ baseUrl, qs: { access_token: TEST_OAUTH_TOKEN } });
 
 describe('Voting', () => {
-  let projectId;
+  let hackathonId;
+  let ourProjectId;
+  let theirProjectId;
   let personId;
   let teamId;
 
   before(() => {
-    return chakram.post('/team', { name: 'A-Team' })
+    return chakram.post('/hackathon', {
+      'name': 'Hackathon',
+      'startDate': 1484678880990,
+      'endDate': 1484678880991
+    })
+    .then(response => {
+      hackathonId = response.body.id;
+    })
+    .then(() => {
+      return chakram.post('/team', { name: 'A-Team' });
+    })
     .then(response => {
       teamId = response.body.id;
     })
     .then(() => {
       return chakram.post('/project', {
-        name: 'Awesomium',
-        description: 'An epic project',
-        teamId
+        name: 'Their project',
+        description: 'Their project description',
+        hackathonId
       });
     })
     .then(response => {
-      projectId = response.body.id;
+      theirProjectId = response.body.id;
+    })
+    .then(() => {
+      return chakram.post('/project', {
+        name: 'Our project',
+        description: 'Our project description',
+        teamId,
+        hackathonId
+      });
+    })
+    .then(response => {
+      ourProjectId = response.body.id;
     })
     .then(() => {
       return chakram.post('/person', {
         name: 'John Doe',
-        email: 'john.doe@example.com',
+        githubId: TEST_PERSON_GITHUB_ID,
+        email: TEST_PERSON_EMAIL,
         teamId
       });
     })
@@ -48,66 +73,32 @@ describe('Voting', () => {
   });
 
   after(() => chakram.waitFor([
-    chakram.delete(`/project/${projectId}`),
+    chakram.delete(`/project/${ourProjectId}`),
+    chakram.delete(`/project/${theirProjectId}`),
     chakram.delete(`/person/${personId}`),
     chakram.delete(`/team/${teamId}`)
   ]));
 
-  it('should be possible only with valid personId and projectId', () => {
+  it('should be possible only if the person takes participate in hackathon which given project is part.', () => {
     return chakram.post('/vote', {
       points: 5,
-      projectId,
-      personId
+      projectId: theirProjectId
     }).then(response => {
       expect(response).to.have.status(201);
       expect(response).to.contain.json({
         points: 5,
-        projectId,
-        personId
+        projectId: theirProjectId
       });
     });
   });
 
-  it('should be impossible if given project does not have a proper relation with other entities', () => {
+  it('should be impossible to vote on own project.', () => {
     return chakram.post('/vote', {
       points: 5,
-      projectId: uuidV4(),
-      personId
+      projectId: ourProjectId
     }).then(response => {
       expect(response).to.have.status(400);
-      expect(response.body.message).to.match(/Project .* does not exist/);
-    });
-  });
-
-  it('should be impossible if given person does not have a proper relation with other entities', () => {
-    return chakram.post('/vote', {
-      points: 5,
-      projectId,
-      personId: uuidV4()
-    }).then(response => {
-      expect(response).to.have.status(400);
-      expect(response.body.message).to.match(/Given person is not a member/);
-    });
-  });
-
-  it('should be impossible if any of required relations is missing', () => {
-    return chakram.post('/vote', {
-      points: 5,
-      personId
-    }).then(response => {
-      expect(response).to.have.status(400);
-      expect(response.body.message).to.match(/Missing/);
-    });
-  });
-
-  it('should be impossible for the same personId and projectId', () => {
-    return chakram.post('/vote', {
-      points: 5,
-      projectId,
-      personId
-    }).then(response => {
-      expect(response).to.have.status(400);
-      expect(response.body.message).to.match(/Vote .* already exists/);
+      expect(response.body.message).to.match(/own project/);
     });
   });
 });
